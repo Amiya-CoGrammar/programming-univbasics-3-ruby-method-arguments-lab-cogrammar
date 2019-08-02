56c471b853d6b603c235c77b9a925121a4985cef
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-02 14:15:39 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,17 @@
+def introduction(name)
+  puts "Hi name #{name}"
+end
+
+introduction("Dan")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Ryan", "React")
+
+def introduction_with_language_optional(name, language*)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Lyah")
