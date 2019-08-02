aca74174c577fb4b023147a1da7aa29f0c96eb63
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-02 14:20:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,8 +10,8 @@ end
 
 introduction_with_language("Ryan", "React")
 
-def introduction_with_language_optional(name, *language)
+def introduction_with_language_optional(name, language="Ruby")
     puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language_optional("Lyah", nil)
+introduction_with_language_optional("Lyah")
